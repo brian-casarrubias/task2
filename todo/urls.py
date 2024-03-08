2f9ba12be2b82112475a80acc9979541defa7eb4
brian <briancasarrubias@gmail.com>
2024-03-08 13:44:30 -0500

    added the sign up

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,12 @@ from todo import views
 urlpatterns = [
    path('', views.home, name='home-page'),
    path('register/', views.register, name='register-page'),
+   
 ]
+
+
+htmx_patterns = [
+    path('check_available/', views.check_user_availability, name='check-available-page'),
+]
+
+urlpatterns += htmx_patterns
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+#forms
+from todo import forms
 
 # Create your views here.
 
@@ -11,5 +16,28 @@ def home(request):
 
 
 def register(request):
-    context={}
+
+    if request.method == 'POST':
+        form = forms.UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home-page')
+        
+    else:
+        form = forms.UserRegisterForm()
+
+
+    context={
+        'form':form,
+    }
     return render(request, 'todo/register.html', context)
+
+def check_user_availability(request):
+    username = request.POST.get('username')
+    if get_user_model().objects.filter(username=username).exists():
+        return HttpResponse('<p style="color:red" > Not available </p>')
+    
+    else:
+        return HttpResponse('<p style="color:green" > Available </p>')
+   
